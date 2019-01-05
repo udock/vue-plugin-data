1a34a09e6aee5792d8eccbc55b0171ddca012be0
vaniship <vaniship@gmail.com>
2019-01-05 10:00:20 +0800

    fix: fix build tools config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   external: (id) => /^lodash\/.*$/.test(id),
   globals: {
-    'babel-runtime/core-js/get-iterator': 'core.getIterator',
-    'babel-runtime/core-js/symbol': 'core.Symbol',
-    'babel-runtime/helpers/slicedToArray': 'BabelHelpers.slicedToArray',
+    '@babel/runtime/helpers/slicedToArray': 'BabelHelpers.slicedToArray',
     'lodash/extend': '_.extend',
     'lodash/forOwn': '_.forOwn',
     'lodash/get': '_.get',
